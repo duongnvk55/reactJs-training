e18c79f433f242b197faef75dcb7d4fb033bca55
Jackie <jackie.d@shopstack.asia>
2019-02-28 13:59:35 +0700

    Updates

---
+++ precache-manifest.5e50e5cb1b7456d89e6b2f50d54799dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactJs-training/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "514fe3dcc424c09aee7f",
+    "url": "/reactJs-training/static/js/runtime~main.514fe3dc.js"
+  },
+  {
+    "revision": "086919c403931ecd0405",
+    "url": "/reactJs-training/static/js/main.086919c4.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/reactJs-training/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "086919c403931ecd0405",
+    "url": "/reactJs-training/static/css/main.cef2b6b1.chunk.css"
+  },
+  {
+    "revision": "8dec3427bf81d684d818e9f3c2ac1fce",
+    "url": "/reactJs-training/index.html"
+  }
+];
